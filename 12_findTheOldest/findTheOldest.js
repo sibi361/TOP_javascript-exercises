21a6c8b4c869947ffa09a1f8011cfc172424104e
sibi361 <104310887+sibi361@users.noreply.github.com>
2023-05-18 18:10:52 +0530

    Finished Level 12 And `archived_caesar`

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleDict) {
+  let peopleNew = [];
+  peopleDict.forEach((element) => {
+    let eleNew = {};
+    let age;
 
+    eleNew.name = element.name;
+    if (element.yearOfDeath != undefined)
+      age = element.yearOfDeath - element.yearOfBirth;
+    else age = new Date().getFullYear() - element.yearOfBirth;
+    eleNew.age = age;
+
+    peopleNew.push(eleNew);
+  });
+
+  let oldest = {};
+  peopleNew.forEach((element) => {
+    if (oldest.age == undefined) oldest = element;
+    else {
+      if (element.age > oldest.age) oldest = element;
+    }
+  });
+
+  return oldest;
 };
 
 // Do not edit below this line
--- archive/archived_caesar/caesar.js
+++ archive/archived_caesar/caesar.js
@@ -1,6 +1,38 @@
-const caesar = function() {
+const caesar = function (text, shift) {
+  let textNew = "";
 
+  text.split("").forEach((char) => {
+    if (isLetter(char)) {
+      let currentCode = char.charCodeAt();
+      let newCode, offset;
+
+      shift = shift % 26;
+      if (char == char.toUpperCase()) {
+        offset = currentCode + shift - 65;
+        offset = offsetClean(offset, shift);
+        newCode = 65 + offset;
+      } else {
+        offset = currentCode + shift - 97;
+        console.log(offset);
+        offset = offsetClean(offset, shift);
+        newCode = 97 + offset;
+      }
+      textNew += String.fromCharCode(newCode);
+    } else textNew += char;
+  });
+  return textNew;
 };
+
+function isLetter(c) {
+  return c.toLowerCase() != c.toUpperCase();
+}
+
+function offsetClean(o, s) {
+  let offset = o;
+  if (o < 0) offset = s < 0 ? 26 + o : o;
+  else if (offset >= 26) offset = o % 26;
+  return offset;
+}
 
 // Do not edit below this line
 module.exports = caesar;
